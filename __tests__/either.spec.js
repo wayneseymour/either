811b5321b3c175bdc72f205d5f3a86e40535a65e
Tre <wseymour@envysion.com>
2018-05-01 06:11:17 -0600

    init commit

---
+++ __tests__/either.spec.js
@@ -0,0 +1,22 @@
+import {fromNullable, tryCatch} from '../lib';
+
+describe(`either datatype functions`, () => {
+	describe(`'fromNullable'`, () => {
+		it(`should be a fn`, () => {
+			expect(typeof fromNullable).toBe('function');
+		});
+		it(`should drop processing if a false is calculated`, () => {
+			const obj = false;
+			fromNullable(obj)
+				.map(pluck('detail'))
+				.fold(x => expect(x).toEqual(null), x => console.log(`\n### x: \n\t${x}`));
+		});
+	});
+	describe(`'tryCatch'`, () => {
+		it(`should be a fn`, () => {
+			expect(typeof tryCatch).toBe('function');
+		});
+	});
+});
+const pluck = x => obj => obj[x];
+
